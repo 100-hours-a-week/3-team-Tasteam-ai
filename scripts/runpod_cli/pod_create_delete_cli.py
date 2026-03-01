4564d3ac436ba2f16828b049d53635d7b4529e1d
jinsoo <js@jinsoos-MacBook-Pro.local>
2026-03-01 21:37:18 +0900

    refactor: gpu name error

--- scripts/runpod_cli/pod_create_delete_cli.py
+++ scripts/runpod_cli/pod_create_delete_cli.py
@@ -174,7 +174,7 @@ class RunPodClient:
             "env": {"ENV_VAR": "value",**({"WANDB_API_KEY": os.environ["WANDB_API_KEY"]} if os.environ.get("WANDB_API_KEY") else {}),},
             "globalNetworking": False,
             "gpuCount": 1,
-            "gpuTypeIds": ["NVIDIA GeForce RTX 4090","NVIDIA GeForce RTX A5000"],
+            "gpuTypeIds": ["NVIDIA GeForce RTX 4090","NVIDIA RTX A5000"],
             "gpuTypePriority": "availability",
             "imageName": image_name,
             "interruptible": False,
