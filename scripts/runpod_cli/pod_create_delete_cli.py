557f96a79f87be07cdb5c29eadfaebb81ed61f0e
jinsoo <js@jinsoos-MacBook-Pro.local>
2026-03-01 21:28:52 +0900

    refactor: add rtx pro 4500

--- scripts/runpod_cli/pod_create_delete_cli.py
+++ scripts/runpod_cli/pod_create_delete_cli.py
@@ -174,7 +174,7 @@ class RunPodClient:
             "env": {"ENV_VAR": "value",**({"WANDB_API_KEY": os.environ["WANDB_API_KEY"]} if os.environ.get("WANDB_API_KEY") else {}),},
             "globalNetworking": False,
             "gpuCount": 1,
-            "gpuTypeIds": ["NVIDIA GeForce RTX 4090"],
+            "gpuTypeIds": ["NVIDIA GeForce RTX 4090","NVIDIA GeForce RTX PRO 4500"],
             "gpuTypePriority": "availability",
             "imageName": image_name,
             "interruptible": False,
